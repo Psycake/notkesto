ac70eb809f6a46584f792379fc2d17baa371eb20
Luis Mauricio Camargo <52770775+Psycake@users.noreply.github.com>
2022-11-03 19:35:24 -0600

    autopush

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var configured_html_url_prefix = "/notkesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '211071549324799245990314848883433687746'                       // used to check whether the localStorage data is stale
+var gzip_hash = '88424939156667329528928478363714082648'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
