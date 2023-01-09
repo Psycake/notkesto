8199b431218e0386e32ddc0898fb00b3afe07d7b
Luis Mauricio Camargo <52770775+Psycake@users.noreply.github.com>
2023-01-08 21:22:32 -0600

    autopush

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -6,12 +6,12 @@ var toc_pane = 1;
 var mermaid_enabled = 1;
 var toc_pane_div = "right_pane_content";
 var content_pane_div = "left_pane_content";
-var html_url_prefix = "/notkesto";
-var configured_html_url_prefix = "/notkesto";
+var html_url_prefix = "";
+var configured_html_url_prefix = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '88424939156667329528928478363714082648'                       // used to check whether the localStorage data is stale
+var gzip_hash = '162551451274337051454264920357017630256'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
@@ -434,7 +434,7 @@ function get_graph_data(){
     return get_html_url_prefix()+'/obs.html/data/graph.json';
 }
 function get_html_url_prefix(){
-    return '/notkesto'  // this value is replaced by the actual url prefix when compiled
+    return ''  // this value is replaced by the actual url prefix when compiled
 }
 
 function signal_js_enabled(container){
